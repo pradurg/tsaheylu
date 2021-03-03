9c8a52213c48c5a4d0a6e952ba96819c71ee62aa
korde96 <askorde2@gmail.com>
2021-03-03 10:16:28 +0530

    min fixes

--- tsaheylu-health/src/main/java/io/durg/tsaheylu/SigmoidHealthMetricFunction.java
+++ tsaheylu-health/src/main/java/io/durg/tsaheylu/SigmoidHealthMetricFunction.java
@@ -6,23 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Slf4j
 public class SigmoidHealthMetricFunction implements Function<List<MetricMonitor>, Double> {
 
     @Override
     public Double apply(List<MetricMonitor> metricMonitors) {
-        Stream<MetricMonitor> availableMonitors = metricMonitors.stream()
+        double healthMetric = metricMonitors.stream()
                 .filter(Objects::nonNull)
-                .filter(metricMonitor -> metricMonitor.getMetric() != null && metricMonitor.getMetric() != Double.NaN);
-        List<MetricMonitor> monitorList = availableMonitors.collect(Collectors.toList());
-        if (monitorList.size() == 0) {
-            log.warn("No metrics available");
-            return Double.NaN;
-        }
-        double healthMetric = monitorList.stream()
+                .filter(metricMonitor -> metricMonitor.getMetric() != null && metricMonitor.getMetric() != Double.NaN)
                 .map(monitor -> monitor.getWeight() * monitor.getMetric())
                 .map(x -> (1 / (1 + Math.pow(Math.E, (-1 * x)))))
                 .mapToDouble(Double::doubleValue)
