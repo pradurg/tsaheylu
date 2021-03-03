4a5a73c7c8d5d7487eba275ce4a88c1c7db46bdf
korde96 <askorde2@gmail.com>
2021-03-03 17:56:10 +0530

    minor fix

--- tsaheylu-common/src/main/java/io/durg/tsaheylu/model/NodeData.java
+++ tsaheylu-common/src/main/java/io/durg/tsaheylu/model/NodeData.java
@@ -3,13 +3,12 @@ package io.durg.tsaheylu.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class NodeData {
     private String environment;
     private double healthMetric;
